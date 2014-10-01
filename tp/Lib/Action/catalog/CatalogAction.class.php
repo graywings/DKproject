ce78c13dc92a3d1d9e5283ae5bcb087250e9d422
PaulKim725 <monsterdark725@gmail.com>
2014-10-01 16:11:20 +0800

    catalogAction 漏提交。

---
+++ tp/Lib/Action/catalog/CatalogAction.class.php
@@ -0,0 +1,48 @@
+<?php
+class CatalogAction extends BaseAction
+{
+
+	public function index()
+	{
+		$this->display();
+	}
+
+	/**
+	 * 查询可专注的分类
+	 * 只查询目录类型为1的所有分类
+	 */
+	public function getFollowCatalogs()
+	{
+		$cat = new MongoModel( "cate" );
+		$this->data = $cat->field( "url,name,code" )->select( array(
+			'where' => array(
+				"type" => "1" 
+			),
+			"order" => array(
+				"num" => 1 
+			) 
+		) );
+		
+		$this->ajax( $this->data );
+	}
+
+	/**
+	 * 查询所有分类，可能包括 Popular，Everything，Other等目录
+	 * 包括目录类型为1的和其他的。
+	 */
+	public function getPageCatalogs()
+	{
+		$cat = new MongoModel( " cate " );
+		$this->data = $cat->field( "url,name,code" )->select( array(
+			'where' => array(
+				"type" => "1" 
+			),
+			"order" => array(
+				"num" => 1 
+			) 
+		) );
+		
+		$this->ajax( $this->data );
+	}
+
+}
